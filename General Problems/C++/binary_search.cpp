199d449f25881d8829bd2938f451a67357b1dba9
Petru Boscu <petruboscu@yahoo.com>
2022-09-25 10:40:25 +0300

    Fix binary_search.cpp from General Problems

--- General Problems/C++/binary_search.cpp
+++ General Problems/C++/binary_search.cpp
@@ -6,13 +6,18 @@ int binary_search(int orderedElements[], int number_of_elements, int element);
 
 int main()
 {
-    int list[12] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-    int element = 10;
-    int index = binary_search(list, 11, element);
-    if (index != -1)
-        cout << "Element " << element << " is present on position " << index <<".";
-    else
-        cout << "Element " << element << " is not present.";
+    int list[10] = {1, 2, 4, 5, 6, 7, 9, 10, 11};
+    int listLength = 9;
+
+    int index;
+    for (int element = 1; element <= 11; element++)
+    {
+        index = binary_search(list, listLength, element);
+        if (index != -1)
+            cout << "Element " << element << " is present on position " << index << "." << endl;
+        else
+            cout << "Element " << element << " is not present." << endl;
+    }
 }
 
 int binary_search(int orderedElements[], int number_of_elements, int element)
@@ -21,13 +26,12 @@ int binary_search(int orderedElements[], int number_of_elements, int element)
         return -1;
 
 
-    int left = 0;
-    int right = number_of_elements - 1;
+    int left = -1;
+    int right = number_of_elements;
     int middle = (left + right) / 2;
 
-    while(orderedElements[middle] != element)
+    while (orderedElements[middle] != element)
     {
-        cout << middle;
         if (element > orderedElements[middle])
         {
             left = middle;
@@ -38,7 +42,7 @@ int binary_search(int orderedElements[], int number_of_elements, int element)
             right = middle;
             middle = (left + right) / 2;
         }
-        if(left == right)
+        if(left == right - 1)
             return -1;
     }
     return middle;
